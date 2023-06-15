dba02aee89d066eda5f9a45da4cc6fd635a8f11c
XuC <hannable@163.com>
2023-06-16 00:03:23 +0800

    change something in cpp file

--- test_file.cpp
+++ test_file.cpp
@@ -5,6 +5,8 @@ int main(int argc, char** argv)
 {
     int i = 0;
 
+	auto img = cv::imread("C:\\Download\\test.jpg");
+
     for(i; i < 30; i++)
     {
         std::cout << "hello, world!\n" << std::endl;
